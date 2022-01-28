268f6f4e706f5bb301ea4727014ad985bf1f34aa
Zineb Bendhiba <bendhiba.zineb@gmail.com>
2022-01-28 10:00:32 +0100

    init

--- src/main/java/org/acme/Routes.java
+++ src/main/java/org/acme/Routes.java
@@ -1,48 +1,11 @@
 package org.acme;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-
-import org.acme.model.TelegramMessage;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.telegram.TelegramConstants;
-import org.apache.camel.component.telegram.TelegramParseMode;
 
-@ApplicationScoped
 public class Routes extends RouteBuilder {
-    @Inject
-    TelegramBean myBean;
 
     @Override
     public void configure() throws Exception {
-        from("telegram:bots?authorizationToken={{telegram-token-api}}")
-                .bean(myBean, "translate")
-                .to("direct:process-message");
-
-        from("direct:process-message")
-                .choice()
-                    .when(simple("${body} contains '/start'"))
-                        .transform(simple("{{msg.bot.start}}"))
-                    .when(simple("${body} contains '#msg'"))
-                        .to("kafka:telegram-message")
-                        .transform(simple("{{msg.bot.msg}}"))
-                    .otherwise()
-                        .transform(simple("{{msg.bot.otherwise}}"))
-                .end()
-                .setHeader(TelegramConstants.TELEGRAM_PARSE_MODE, simple(TelegramParseMode.MARKDOWN.name()))
-                .to("telegram:bots?authorizationToken={{telegram-token-api}}");
-
-        from("kafka:telegram-message")
-                .log("Incoming message from Kafka topic telegram-message ${body} ")
-                .unmarshal().json(TelegramMessage.class)
-                .to("jpa:"+ TelegramMessage.class)
-                .bean(myBean, "createNotification")
-                .toD("slack://general?webhookUrl={{webhook-url}}");
-
-        from("platform-http:/messages?httpMethodRestrict=GET")
-                .to("jpa:"+ TelegramMessage.class+"?namedQuery=findAll")
-                .marshal().json();
-
 
     }
 }
