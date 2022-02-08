5406c3c996085eeeb82c30eba2f4899db7a548eb
Zineb Bendhiba <bendhiba.zineb@gmail.com>
2022-02-08 16:28:51 +0100

    change wording for Paris jug

--- src/main/java/org/acme/Routes.java
+++ src/main/java/org/acme/Routes.java
@@ -14,10 +14,6 @@ public class Routes extends RouteBuilder {
     public void configure() throws Exception {
         from("telegram:bots?authorizationToken={{telegram-token-api}}")
                 .to("atlasmap:atlasmap-mapping.adm")
-                .log("${body}")
-                .to("direct:process-message");
-
-        from("direct:process-message")
                 .choice()
                     .when(simple("${body} contains '/start'"))
                         .transform(simple("{{msg.bot.start}}"))
@@ -27,7 +23,6 @@ public class Routes extends RouteBuilder {
                     .otherwise()
                         .transform(simple("{{msg.bot.otherwise}}"))
                 .end()
-                .setHeader(TelegramConstants.TELEGRAM_PARSE_MODE, simple(TelegramParseMode.MARKDOWN.name()))
                 .to("telegram:bots?authorizationToken={{telegram-token-api}}");
 
         from("kafka:telegram-message")
