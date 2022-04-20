a04cbc76c1bc3c75aaf9ddfd6c97f736c7cb9658
Zineb Bendhiba <bendhiba.zineb@gmail.com>
2022-04-20 15:04:59 +0200

    intermediate

--- src/main/java/org/acme/Routes.java
+++ src/main/java/org/acme/Routes.java
@@ -9,15 +9,8 @@ public class Routes extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("telegram:bots")
-                .bean("telegramBean", "translate")
-                .to("kafka:telegram-topic")
                 .transform(constant("Merci pour votre message!"))
                 .to("telegram:bots");
 
-        from("kafka:telegram-topic")
-                .log("incoming message ${body}")
-                .unmarshal().json(TelegramMessage.class)
-                .bean("telegramBean",  "generateNotification")
-                .to("slack:#myslack");
     }
 }
