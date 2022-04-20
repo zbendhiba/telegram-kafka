698ec2b803ebabf14d5e9439eb98696d3ee7e619
Zineb Bendhiba <bendhiba.zineb@gmail.com>
2022-04-20 15:06:41 +0200

    init

--- src/main/java/org/acme/Routes.java
+++ src/main/java/org/acme/Routes.java
@@ -8,9 +8,6 @@ import org.apache.camel.builder.RouteBuilder;
 public class Routes extends RouteBuilder {
     @Override
     public void configure() throws Exception {
-        from("telegram:bots")
-                .transform(constant("Merci pour votre message!"))
-                .to("telegram:bots");
 
     }
 }
