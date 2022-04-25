8462a0f36d2fb73fcae9ce09a90bac3bdd27ba68
Zineb Bendhiba <bendhiba.zineb@gmail.com>
2022-04-25 09:36:41 +0200

    Demo live at devoxx FR

--- src/main/java/org/acme/Routes.java
+++ src/main/java/org/acme/Routes.java
@@ -9,8 +9,17 @@ public class Routes extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("telegram:bots")
+                .bean("telegramBean", "translate")
+                .to("kafka:toto")
                 .transform(constant("Merci pour votre message!"))
                 .to("telegram:bots");
+
+        from("kafka:toto")
+                .log("Incoming from kafka ${body}")
+                .unmarshal().json(TelegramMessage.class)
+                .bean("telegramBean", "generateNotification")
+                .log("${body}")
+                .to("slack:#myslack");
 
     }
 }
