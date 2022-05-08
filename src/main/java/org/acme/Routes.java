5a68cf352544afd0944a45559e2221cc6f8f80cd
Zineb Bendhiba <bendhiba.zineb@gmail.com>
2022-05-08 21:28:40 +0200

    Input to dynamo DB is correct

--- src/main/java/org/acme/Routes.java
+++ src/main/java/org/acme/Routes.java
@@ -1,28 +1,36 @@
 package org.acme;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.acme.model.TelegramMessage;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.aws2.ddb.Ddb2Constants;
 import org.apache.camel.component.aws2.ddb.Ddb2Operations;
 import org.apache.camel.component.telegram.TelegramConstants;
 import org.apache.camel.component.telegram.TelegramParseMode;
+import org.apache.camel.component.telegram.model.IncomingMessage;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 
 public class Routes extends RouteBuilder {
+    private static final String ID_COLUMN_NAME = "id";
+    private static final String TEXT_COLUMN_NAME = "text";
+    private static final String USERNAME_COLUMN_NAME = "userName";
 
     @Override
     public void configure() throws Exception {
 
         from("telegram:bots?authorizationToken={{telegram-token-api}}")
-                .bean("telegramBean", "toKafka")
+                .bean(TelegramBean.class)
                 .to("direct:process-message");
 
         from("direct:process-message")
                 .choice()
                     .when(simple("${body} contains '/start'"))
                         .transform(simple("{{msg.bot.start}}"))
-                    .when(simple("${body} contains '#msg'"))
-                        .to("kafka:telegram-message")
-                        .transform(simple("{{msg.bot.msg}}"))
                     .otherwise()
+                        .to("kafka:telegram-message")
                         .transform(simple("{{msg.bot.otherwise}}"))
                 .end()
                 .setHeader(TelegramConstants.TELEGRAM_PARSE_MODE, simple(TelegramParseMode.MARKDOWN.name()))
@@ -30,16 +38,24 @@ public class Routes extends RouteBuilder {
 
         from("kafka:telegram-message")
                 .log("Incoming message from Kafka topic telegram-message ${body} ")
-               .unmarshal().json(TelegramMessage.class)
-                .bean("telegramBean", "toDynamo")
+                .unmarshal().json(TelegramMessage.class)
+                .process(e-> {
+                    TelegramMessage message = e.getMessage().getBody(TelegramMessage.class);
+                    Map<String, Object> item = createItem(message, ID_COLUMN_NAME, TEXT_COLUMN_NAME, USERNAME_COLUMN_NAME);
+                    e.getMessage().setHeader(Ddb2Constants.CONSISTENT_READ, true);
+                    e.getMessage().setHeader(Ddb2Constants.ITEM, item);
+                })
                 .log("transformed for Dynamo ${body}")
                 .to("aws2-ddb://telegram?operation="+ Ddb2Operations.PutItem)
             ;
 
-      /*  from("platform-http:/messages?httpMethodRestrict=GET")
-                .to("jpa:"+ TelegramMessage.class+"?namedQuery=findAll")
-                .marshal().json();*/
+    }
 
-
+    static Map<String, Object> createItem(TelegramMessage message, String idColumnName, String textColumnName, String usernameColumnName) {
+        Map<String, Object> item = new HashMap<>();
+        item.put(idColumnName, AttributeValue.builder().s(message.getId()).build());
+        item.put(textColumnName, AttributeValue.builder().s(message.getText()).build());
+        item.put(usernameColumnName, AttributeValue.builder().s(message.getUserName()).build());
+        return item;
     }
 }
--- src/main/java/org/acme/TelegramBean.java
+++ src/main/java/org/acme/TelegramBean.java
@@ -1,20 +1,13 @@
 package org.acme;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
 
 import io.quarkus.runtime.annotations.RegisterForReflection;
-import org.acme.model.TelegramMessage;
-import org.apache.camel.component.aws2.ddb.Ddb2Constants;
 import org.apache.camel.component.telegram.model.IncomingMessage;
-import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 
 @ApplicationScoped
 @RegisterForReflection
-@Named("telegramBean")
 public class TelegramBean {
 
     private static final String ID_COLUMN_NAME = "id";
@@ -22,25 +15,13 @@ public class TelegramBean {
     private static final String USERNAME_COLUMN_NAME = "userName";
 
 
-    public String toKafka(IncomingMessage incomingMessage) {
+    public String transformToJson(IncomingMessage incomingMessage) {
         return String.format(
                 "{\"id\":\"%s\", \"text\":\"%s\",\"userName\":\"%s\"}",
                 incomingMessage.getMessageId(),
                 incomingMessage.getText(),
                 incomingMessage.getFrom().getFirstName() + " " + incomingMessage.getFrom().getLastName());
-
     }
 
-    public Map toDynamo(TelegramMessage message){
-        Map<String, Object> item = new HashMap<>();
-        item.put(ID_COLUMN_NAME, AttributeValue.builder().s(message.getId()).build());
-        item.put(TEXT_COLUMN_NAME, AttributeValue.builder().s(message.getText()).build());
-        item.put(USERNAME_COLUMN_NAME, AttributeValue.builder().s(message.getUserName()).build());
-        return new HashMap<>() {
-            {
-                put(Ddb2Constants.CONSISTENT_READ, true);
-                put(Ddb2Constants.ITEM, item);
-            }
-        };
-    }
+
 }
