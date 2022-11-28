e77e025299d02f262225667245dd3d8244e6bb64
Zineb Bendhiba <bendhiba.zineb@gmail.com>
2022-11-28 10:28:05 +0100

    final

--- src/main/java/org/acme/camel/demo/Routes.java
+++ src/main/java/org/acme/camel/demo/Routes.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.UUID;
 
 import com.google.api.services.sheets.v4.model.ValueRange;
+import org.acme.camel.demo.pojo.TelegramMessage;
 import org.apache.camel.Message;
 import org.apache.camel.Predicate;
 import org.apache.camel.builder.RouteBuilder;
@@ -21,37 +22,37 @@ public class Routes extends RouteBuilder {
         from("telegram:bots")
                 .choice()
                     .when(isStart)
-                        .transform(simple("{{msg.bot.start}}"))
+                        .transform(constant("{{msg.bot.start}}"))
                     .otherwise()
                         .bean(TelegramService.class)
                         .marshal().json()
-                    .to("kafka:telegram-message")
-                        .transform(simple("{{msg.bot.msg}}"))
+                        .to("kafka:telegram-message")
+                        .transform(constant("{{msg.bot.msg}}"))
                     .end()
                 .to("telegram:bots");
 
         from("kafka:telegram-message")
                 .log("Incoming message from Kafka topic telegram-message ${body}")
-                .setHeader(AWS2S3Constants.KEY, simple(UUID.randomUUID().toString()))
+                .setHeader(AWS2S3Constants.KEY, () -> UUID.randomUUID().toString())
                 .log(String.format("Sending message with header :: ${header.%s}", AWS2S3Constants.KEY))
                 .to("aws2-s3:{{aws-s3.bucket-name}}");
 
 
         from("aws2-s3:{{aws-s3.bucket-name}}")
                 .log("${body}")
-                .unmarshal().json(JsonLibrary.Jackson, java.util.Map.class)
+                .unmarshal().json(TelegramMessage.class)
                 .log("unmarshalled: ${body}")
                 .process(exchange -> {
                     final Message m = exchange.getMessage();
-                    Map<String, Object> body = m.getBody(Map.class);
+                    TelegramMessage body = m.getBody(TelegramMessage.class);
                     m.setHeader("CamelGoogleSheets.valueInputOption", "RAW");
                     m.setHeader("CamelGoogleSheets.values",
                             new ValueRange().setValues(
                                     Arrays.asList(
                                             Arrays.asList(
-                                                    body.get("dateTime"),
-                                                    body.get("userName"),
-                                                    body.get("text")
+                                                    body.getDateTime(),
+                                                    body.getUserName(),
+                                                    body.getText()
                                             ))));
 
                 })
