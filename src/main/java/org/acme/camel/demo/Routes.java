b0ad2eca7621368eee121dc3884560b88a0b6c1a
Zineb Bendhiba <bendhiba.zineb@gmail.com>
2022-11-28 12:12:16 +0100

    EndpointRouteBuilder

--- src/main/java/org/acme/camel/demo/Routes.java
+++ src/main/java/org/acme/camel/demo/Routes.java
@@ -9,36 +9,38 @@ import org.acme.camel.demo.pojo.TelegramMessage;
 import org.apache.camel.Message;
 import org.apache.camel.Predicate;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.endpoint.EndpointRouteBuilder;
 import org.apache.camel.component.aws2.s3.AWS2S3Constants;
 import org.apache.camel.model.dataformat.JsonLibrary;
 
-public class Routes extends RouteBuilder {
+public class Routes extends EndpointRouteBuilder {
 
     @Override
     public void configure() throws Exception {
 
         Predicate isStart = jsonpath("$.text").isEqualTo("/start");
 
-        from("telegram:bots")
+        from(telegram("bots"))
                 .choice()
                     .when(isStart)
                         .transform(constant("{{msg.bot.start}}"))
                     .otherwise()
                         .bean(TelegramService.class)
                         .marshal().json()
-                        .to("kafka:telegram-message")
+                        .to(kafka("telegram-message"))
                         .transform(constant("{{msg.bot.msg}}"))
                     .end()
                 .to("telegram:bots");
 
-        from("kafka:telegram-message")
+        from(kafka("telegram-message"))
                 .log("Incoming message from Kafka topic telegram-message ${body}")
                 .setHeader(AWS2S3Constants.KEY, () -> UUID.randomUUID().toString())
                 .log(String.format("Sending message with header :: ${header.%s}", AWS2S3Constants.KEY))
-                .to("aws2-s3:{{aws-s3.bucket-name}}");
+                .to(aws2S3("{{aws-s3.bucket-name}}"));
 
 
-        from("aws2-s3:{{aws-s3.bucket-name}}?delay=1500")
+        from(aws2S3("{{aws-s3.bucket-name}}")
+                    .delay(1500))
                 .log("${body}")
                 .unmarshal().json(TelegramMessage.class)
                 .process(exchange -> {
